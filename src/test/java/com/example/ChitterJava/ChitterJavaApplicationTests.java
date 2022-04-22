67b3abe232150d1be5773e922850859942be731d
Xfarya <farya243@gmail.com>
2022-04-22 15:55:48 +0100

    initial commit, set up app using spring boot

---
+++ src/test/java/com/example/ChitterJava/ChitterJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ChitterJava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChitterJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
