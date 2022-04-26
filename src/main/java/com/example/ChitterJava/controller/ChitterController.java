e4678f4bba6b22844c73e574a9a5300144f4b5ce
Xfarya <farya243@gmail.com>
2022-04-26 14:12:52 +0100

    wrote initial test for getcontroller, working

--- src/main/java/com/example/ChitterJava/controller/ChitterController.java
+++ src/main/java/com/example/ChitterJava/controller/ChitterController.java
@@ -1,14 +1,16 @@
 package com.example.ChitterJava.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
+@RequestMapping
 public class ChitterController {
 
-    @RequestMapping(value="peeps")
-    @ResponseBody
+    @GetMapping(value="/peeps")
     public String peepPage() {
         return "Hello!";
     }
--- src/test/java/com/example/ChitterJava/controller/ChitterControllerTest.java
+++ src/test/java/com/example/ChitterJava/controller/ChitterControllerTest.java
@@ -6,25 +6,39 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.client.match.ContentRequestMatchers;
+import org.springframework.test.web.servlet.*;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-import static org.hamcrest.Matchers.hasSize;
-import static org.springframework.test.web.client.match.MockRestRequestMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import java.net.URI;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(SpringExtension.class)
-@WebMvcTest
-class ChitterControllerTest {
+@WebMvcTest(controllers = ChitterController.class)
+public class ChitterControllerTest {
+
     @Autowired
-    MockMvc mockMvc;
+    private MockMvc mockMvc;
 
     @MockBean
     private ChitterService chitterService;
 
     @Test
-    void getAllPeeps() throws Exception {
-
+    public void getAllPeeps() throws Exception {
+        mockMvc.perform(get("/peeps")).andExpect(status().is2xxSuccessful());
+        RequestBuilder request = MockMvcRequestBuilders
+                .get("/peeps")
+                .accept(MediaType.APPLICATION_JSON);
+        MvcResult result = mockMvc.perform(request).andReturn();
+        assertEquals("Hello!", result.getResponse().getContentAsString());
     }
 
 }
